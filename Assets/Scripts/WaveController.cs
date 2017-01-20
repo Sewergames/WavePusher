11e31eb3e85fafd42a10905c9ffd202bc1ce0302
Linas Nikiperavicius <nikiperaviciuslinas@gmail.com>
2017-01-20 20:53:59 +0200

    Initial Project Setup

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (LineRenderer))]
+public class WaveController : MonoBehaviour {
+	LineRenderer lineRen;
+
+
+	// Use this for initialization
+	void Start () {
+		lineRen = getComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
