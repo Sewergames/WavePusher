2463d0d7e4ff6b41b002d9886a0875f0290bf9eb
Linas Nikiperavicius <nikiperaviciuslinas@gmail.com>
2017-01-21 18:30:53 +0200

    lhk

--- Assets/Scripts/AppleController.cs
+++ Assets/Scripts/AppleController.cs
@@ -3,27 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleController : MonoBehaviour {
-    public List<string> collisionTags;
 
 	void Start () {
-        collisionTags = new List<string>();
 
     }
 	
 	void Update ()
     {
-        //Debug.Log(collisionTags.Count);
-    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        string tag = collision.collider.tag == "Wave" ? "Wave" : "Wall";
-        collisionTags.Add(tag);
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        string tag = collision.collider.tag == "Wave" ? "Wave" : "Wall";
-        collisionTags.Remove(tag);
     }
 }
